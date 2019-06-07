275b4d44ef9645878be4e40c17c8907235da5630
Daniel Sava <danielsavas@gmail.com>
2019-06-07 19:25:16 -0300

    Estudos

---
+++ basico/console_clear.py
@@ -0,0 +1,4 @@
+import os
+
+clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+clear()
---
+++ basico/random.py
@@ -0,0 +1,17 @@
+'''
+    Gerar números randômicos
+'''
+
+import random
+
+numr_rand = random.random()
+print(numr_rand)
+
+numr_rand_1 = random.random() * 100
+print(numr_rand_1)
+
+
+# Inteiros : com a função 'built´in' ele arredonda o número
+
+numr_rand_inteiro = round(numr_rand_1)
+print(numr_rand_inteiro)
