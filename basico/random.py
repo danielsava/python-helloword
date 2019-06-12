b413bd4452c67817f02fec99f517e6be1d2788e9
Daniel Sava <danielsavas@gmail.com>
2019-06-12 14:59:49 -0300

    Estudo: Random

--- basico/random.py
+++ basico/random.py
@@ -1,7 +1,3 @@
-'''
-    Gerar números randômicos
-'''
-
 import random
 
 numr_rand = random.random()
@@ -12,6 +8,5 @@ print(numr_rand_1)
 
 
 # Inteiros : com a função 'built´in' ele arredonda o número
-
 numr_rand_inteiro = round(numr_rand_1)
 print(numr_rand_inteiro)
