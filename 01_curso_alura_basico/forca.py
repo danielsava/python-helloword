c3ff8f423a8191ce2e22b41017c96ca09fcd521f
Daniel Sava <danielsavas@gmail.com>
2019-06-18 15:35:53 -0300

    Curso Alura parte 2

--- 01_curso_alura_basico/forca.py
+++ 01_curso_alura_basico/forca.py
@@ -1,10 +1,37 @@
+import random
+
+
 def jogar():
     print("")
     print("**********************************")
     print("*** Bem vindo ao jogo de Força ***")
     print("**********************************", end="\n\n")
 
-    palavra_secreta = "banana"
+    # Somente para criar e preencher o arquivo
+    arquivo = open('palavras.txt', 'w')
+
+    arquivo.write('siriguela\n')
+    arquivo.write('pequi\n')
+    arquivo.write('cagaita\n')
+    arquivo.write('araticum\n')
+    arquivo.write('mangaba\n')
+    arquivo.write('buriti\n')
+    arquivo.write('baru\n')
+
+    arquivo.close()
+
+    # Arquivo modo leitura
+    arquivo = open('palavras.txt', 'r')
+    palavras = []
+    for linha in arquivo:
+        palavras.append(linha.strip().lower())
+    arquivo.close()
+
+    #
+    print("Palavras Secretas: ", palavras, end="\n\n")
+
+    # Escolhendo uma Palavra
+    palavra_secreta = palavras[random.randrange(0, len(palavras))]
 
     print("Palavra Secreta:", palavra_secreta, end="\n\n")
 
@@ -22,7 +49,7 @@ def jogar():
         if chute in palavra_secreta:
             index = 0
             for letra in palavra_secreta:
-                if chute == letra.lower().strip():
+                if chute == letra.strip().lower():
                     print("Encontrei a letra '{}' na posição '{}'".format(letra, index), end="\n\n")
                     letras_acertadas[index] = letra
                 index += 1
