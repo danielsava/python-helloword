777affb2fd5a0cbbdc422df83c31724ec013e4ba
Daniel Sava <danielsavas@gmail.com>
2019-06-24 19:24:40 -0300

    Curso Alura Python OO

--- conta.py
+++ conta.py
@@ -1,11 +1,20 @@
 """
 
-    Para o paradigma de Orientação à Objetos, o python prega como boa prática o padrão CamelCase
+    Atributo Privado
+    '__' : Dois underscore em Python é o 'private' do Java. Ou seja, caso queira que uma atributo seja privado
+           basta adicionar esses dois underscore no nome do atributo.
+
+    Limpando referência de Memória
+    'None': limpa a referência de um objeto
+        - conta = new Conta(1, 'João', 500.0)
+        - c1 = conta
+        - conta = None          # Limpou a referência de 'conta'. Está 'vazia'
 
 """
 
 
 class Conta:
+
 
     """
         Construtores
@@ -13,13 +22,32 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo Conta {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def __init__(self, numero, titular, saldo, limite=1000.0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+
+    """
+        Métodos
+    """
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def imprimir_extrato(self):
+        print("Saldo de {} do titular {}.".format(self.__saldo, self.__titular))
+
